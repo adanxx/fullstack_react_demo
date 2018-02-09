b834cd10b8f61bed9e6bb09de0bf712b7bbb071d
A.D-47 <abdi0361@stud.kea.dk>
2018-02-09 10:21:48 +0100

    Update:  Section. lectrue.28-43 - Done - Testing Logout OutUser

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require("express");
-require('./services/passport')
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require("./config/keys");
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.databaseKey);
+
+require('./services/passport');
+require('./models/User');
 
 const app = express();
+
+app.use(
+    cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [keys.cookieKeys]
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
 
 require('./routes/authtRoutes')(app);
 
